6df764a78375309b7aba6c93d1304ef3ff7551fb
CHINA.lining <aninstar@outlook.com>
2020-08-27 16:26:55 +0800

    更新后台项目

--- koa_server/middleware/koa_response_data.js
+++ koa_server/middleware/koa_response_data.js
@@ -1,22 +1,23 @@
-/**
- * 业务逻辑中间件,读取json文件
- */
-const path = require("path");
-const fileUtils = require("../utils/file_utils");
+// 处理业务逻辑的中间件,读取某个json文件的数据
+const path = require('path')
+const fileUtils = require('../utils/file_utils')
 module.exports = async (ctx, next) => {
-  const url = ctx.request.url;
-  let filePath = url.replace("/api", "");
-  filePath = "../data" + filePath + ".json";
-  filePath = path.join(__dirname, filePath);
+  // 根据url
+  const url = ctx.request.url // /api/seller   ../data/seller.json
+  let filePath = url.replace('/api', '') //  /seller
+  filePath = '../data' + filePath + '.json'  // ../data/seller.json
+  filePath = path.join(__dirname, filePath)
   try {
-    const ret = await fileUtils.getFileJsonData(filePath);
-    ctx.response.body = ret;
+    const ret = await fileUtils.getFileJsonData(filePath)
+    ctx.response.body = ret
   } catch (error) {
     const errorMsg = {
-      message: "读取文件内容失败，文件资源不存在",
-      status: 404,
-    };
-    ctx.response.body = JSON.stringify(errorMsg);
+      message: '读取文件内容失败, 文件资源不存在',
+      status: 404
+    }
+    ctx.response.body = JSON.stringify(errorMsg)
   }
-  await next();
-};
+ 
+  console.log(filePath)
+  await next()
+}
--- koa_server/middleware/koa_response_duration.js
+++ koa_server/middleware/koa_response_duration.js
@@ -1,14 +1,13 @@
-/**
- * 计算服务器消耗时长中间件
- */
+// 计算服务器消耗时长的中间件
 module.exports = async (ctx, next) => {
   // 记录开始时间
-  const start = Date.now();
-  // 让中间件得到执行
-  await next();
-  // 记录结束时间
-  const end = Date.now();
+  const start = Date.now()
+  // 让内层中间件得到执行
+  await next()
+  // 记录结束的时间
+  const end = Date.now()
   // 设置响应头 X-Response-Time
-  const duration = end - start;
-  ctx.set("X-Response-Time", duration + "ms");
-};
+  const duration = end - start
+  // ctx.set 设置响应头
+  ctx.set('X-Response-Time', duration + 'ms')
+}
